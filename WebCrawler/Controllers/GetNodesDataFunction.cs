b19a889612be213a84d671fe709795f1b879b4fe
Jan Růžička <honza.ruzba@seznam.cz>
2024-08-23 18:34:35 +0200

    Add feature Get Links from Url

--- WebCrawler/Controllers/GetNodesDataFunction.cs
+++ WebCrawler/Controllers/GetNodesDataFunction.cs
@@ -1,24 +1,88 @@
-﻿using WebCrawler.Models;
+﻿using HtmlAgilityPack;
+using WebCrawler.Models;
 
-namespace WebCrawler.Controllers
+public class GetNodesDataFunction
 {
-    public class GetNodesDataFunction
+    public Node GetNextNode(Uri url)
     {
+        List<Uri> links = new List<Uri>();
 
-        public Node GetNextNode(string url)
+        try
         {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
 
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(response.Content.ReadAsStringAsync().Result);
 
-            return new Node(
-                
-                "https://www.youtube.com/", 
-
-                new List<string> { 
-                    "https://www.youtube.com/watch?v=o7xxVbRWT98", 
-                    "https://www.youtube.com/watch?v=58pyYC3brZc"
-                }
-                
-                );
+                links = GetAllLinks(htmlDocument, url);
+                CorrectUrls(links);
+            }
         }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Error fetching data: {e.Message}");
+        }
+
+        return new Node(url, links);
+    }
+
+    private List<Uri> GetAllLinks(HtmlDocument htmlDocument, Uri baseUrl)
+    {
+        List<Uri> links = new List<Uri>();
+
+        HtmlNodeCollection hrefNodes = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+
+        if (hrefNodes == null)
+        {
+            return links;
+        }
+
+        foreach (var node in hrefNodes)
+        {
+            string href = node.GetAttributeValue("href", string.Empty);
+
+            if (string.IsNullOrWhiteSpace(href))
+            {
+                continue;
+            }
+
+            Uri uri;
+
+            if (Uri.TryCreate(href, UriKind.Absolute, out uri))
+            {
+                if (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)
+                {
+                    links.Add(uri);
+                }
+            }
+            else
+            {
+                if (Uri.TryCreate(baseUrl, href, out uri))
+                {
+                    if (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)
+                    {
+                        links.Add(uri);
+                    }
+                }
+            }
+        }
+
+        return links;
+    }
+
+    private void CorrectUrls(List<Uri> links)
+    {
+        HashSet<string> uniqueLinks = new HashSet<string>();
+
+        foreach (Uri link in links)
+        {
+            uniqueLinks.Add(link.ToString());
+        }
+
+        links.Clear();
+        links.AddRange(uniqueLinks.Select(link => new Uri(link, UriKind.Absolute)).ToList());
     }
 }
--- WebCrawler/obj/Debug/net8.0/WebCrawler.AssemblyInfo.cs
+++ WebCrawler/obj/Debug/net8.0/WebCrawler.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCrawler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb86b71a54d6653d9798f4940f35c4deb3283f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45aca57893987d7a868c773cce4b568fa276c651")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCrawler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCrawler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
