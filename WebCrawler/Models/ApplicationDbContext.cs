b63e55a1d20bf98d294a0ec7401bffcdacd2e38c
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-04 13:37:48 +0200

    Update ApplicationDbContext.cs with NodeModel

--- WebCrawler/Models/ApplicationDbContext.cs
+++ WebCrawler/Models/ApplicationDbContext.cs
@@ -1,24 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions;
 using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions;
+using WebCrawler.Views;
 
 namespace WebCrawler.Models
 {
-	public class ApplicationDbContext : DbContext
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-		}
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
 
-		public DbSet<WebsiteRecordModel> WebsiteRecords { get; set; }
+        public DbSet<WebsiteRecordModel> WebsiteRecords { get; set; }
 
-
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<WebsiteRecordModel>().ToTable("WebsiteRecord");
-		}
-
-
-	}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WebsiteRecordModel>().ToTable("WebsiteRecord");
+            modelBuilder.Entity<NodeModel>().ToTable("Node");
+        }
+    }
 }
