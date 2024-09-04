77f9e5ece22cfe522a455095b7c5b3e66cd5a4e1
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-04 13:40:56 +0200

    Update WebsitesCrawlerController.cs with ability to create and read records

--- WebCrawler/Controllers/WebsitesCrawlerController.cs
+++ WebCrawler/Controllers/WebsitesCrawlerController.cs
@@ -1,107 +1,116 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using Dapper;
-using MySql.Data.MySqlClient;
-using WebCrawler.Models;
+using DotNetEnv;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using Npgsql;
+using WebCrawler.Models;
 
 namespace WebCrawler.Controllers
 {
-	public class WebsitesCrawlerController : Controller
-	{
-		// GET: WebsitesCrawler/CreateCrawler
-		public ActionResult CreateCrawler()
-		{
-			return View();
-		}
+    public class WebsitesCrawlerController : Controller
+    {
+        // GET: WebsitesCrawler/CreateCrawler
+        public ActionResult CreateCrawler()
+        {
+            return View();
+        }
 
-		// GET: WebsitesCrawler/EditCrawler
-		public ActionResult EditCrawler()
-		{
-			return View();
-		}
+        // GET: WebsitesCrawler/EditCrawler
+        public ActionResult EditCrawler()
+        {
+            return View();
+        }
 
-		// GET: WebsitesCrawler/Crawlers
-		public ActionResult Crawlers()
-		{
-			var articles = new List<WebsiteRecordModel>
-			{
-				new WebsiteRecordModel {Id = 1},
-				new WebsiteRecordModel{Id = 2, Tags="tag1-tag2-tag3"},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},                new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2, Url = "https://webik.ms.mff.cuni.cz/nswi153/seminar-project-webcrawler.html"},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3, Tags="tag8-tag5-tag5", Url = "https://webik.ms.mff.cuni.cz/nswi153/seminar-project-webcrawler.html"},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3, Url = "https://webik.ms.mff.cuni.cz/nswi153/seminar-project-webcrawler.html"},new WebsiteRecordModel{Id = 4},
-				new WebsiteRecordModel{Id = 2},new WebsiteRecordModel{Id = 3, Tags="tag1-tag2-tag3"},new WebsiteRecordModel{Id = 4}
-			};
+        // GET: WebsitesCrawler/Crawlers
+        public ActionResult Crawlers()
+        {
+            string connectionString = GetConnectionString();
+            ;
+            string sql =
+                @"
+        SELECT ""Id"", ""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags""
+        FROM public.""WebsiteRecord"";
+    ";
 
+            var articles = DataAccess.LoadData<WebsiteRecordModel, dynamic>(
+                sql,
+                new { },
+                connectionString
+            );
 
-			return View(articles);
-		}
+            return View(articles);
+        }
 
-		private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-		public WebsitesCrawlerController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public WebsitesCrawlerController(ApplicationDbContext context)
+        {
+            _context = context;
+            Env.Load();
+        }
 
+        private string GetConnectionString()
+        {
+            string databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+            string databasePass = Environment.GetEnvironmentVariable("DATABASE_PASS");
+            string databaseUser = Environment.GetEnvironmentVariable("DATABASE_USER");
+            string databaseServer = Environment.GetEnvironmentVariable("DATABASE_SERVER");
+            string databasePort = Environment.GetEnvironmentVariable("DATABASE_PORT");
 
+            return $"Host={databaseServer};Port={databasePort};Username={databaseUser};Password={databasePass};Database={databaseName};";
+        }
 
-		// POST: WebsitesCrawler/CreateCrawler
-		[HttpPost]
-		public async Task<IActionResult> CreateWebsiteRecord(WebsiteRecordModel model)
-		{
+        // POST: WebsitesCrawler/CreateCrawler
+        [HttpPost]
+        public async Task<IActionResult> CreateWebsiteRecord(WebsiteRecordModel model)
+        {
+            if (
+                (model.Minutes < 0 || model.Minutes > 60)
+                || (model.Hours < 0 || model.Hours > 24)
+                || model.Days < 0
+                || model.Days > 31
+                || string.IsNullOrWhiteSpace(model.Url)
+                || string.IsNullOrWhiteSpace(model.BoundaryRegExp)
+            )
+            {
+                return (Content($"Record badly filled: {model.Id}"));
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(model.Label))
+                {
+                    model.Label = model.Url;
+                }
 
+                if (string.IsNullOrWhiteSpace(model.Tags))
+                {
+                    model.Tags = null;
+                }
 
+                try
+                {
+                    string connectionString = GetConnectionString();
+                    string sql =
+                        @"
+    INSERT INTO public.""WebsiteRecord"" 
+    (""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags"") 
+    VALUES 
+    (@Url, @BoundaryRegExp, @Days, @Hours, @Minutes, @Label, @IsActive, @Tags);
+";
 
-			try
-			{
-				var testRecord = await _context.WebsiteRecords.FirstOrDefaultAsync(); 
+                    DataAccess.SaveData(sql, model, connectionString);
 
-				if (testRecord != null)
-				{
-					Console.WriteLine($"Úspěšně připojeno k databázi. Nalezený záznam: {testRecord.Id}");
-					return Content($"Úspěšně připojeno k databázi. Nalezený záznam: {testRecord.Id}");
-				}
-				else
-				{
-					Console.WriteLine("Úspěšně připojeno k databázi, ale žádné záznamy nebyly nalezeny.");
-					return Content("Úspěšně připojeno k databázi, ale žádné záznamy nebyly nalezeny.");
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Chyba při připojování k databázi: {ex.Message}");
-				return Content($"Chyba při připojování k databázi: {ex.Message}");
-			}
-		}
-
-
-
-		public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
-		{
-			using (IDbConnection connection = new MySqlConnection(connectionString))
-			{
-				List<T> row = connection.Query<T>(sql, parameters).ToList();
-
-				return row;
-
-			}
-		}
-
-		public void SaveData<T>(string sql, T parameters, string connectionString)
-		{
-			using (IDbConnection connection = new MySqlConnection(connectionString))
-			{
-				connection.Execute(sql, parameters);
-			}
-		}
-	}
+                    Console.WriteLine($"Record added successfully: {model.Id}");
+                    return Content($"Record added successfully: {model.Id}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Chyba při připojování k databázi: {ex.Message}");
+                    return Content($"Chyba při připojování k databázi: {ex.Message}");
+                }
+            }
+        }
+    }
 }
