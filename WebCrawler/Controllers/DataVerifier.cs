d243af8ebbee04bb7a95bd0ae75f3d2e51cd0d55
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-06 15:40:56 +0200

    Update DataVerifier.cs to check record existence

--- WebCrawler/Controllers/DataVerifier.cs
+++ WebCrawler/Controllers/DataVerifier.cs
@@ -1,36 +1,48 @@
+using Npgsql;
 using WebCrawler.Models;
 
 namespace WebCrawler.Controllers
 {
-	public class DataVerifier
-	{
-		public static bool WebsiteRecordVerifier(WebsiteRecordModel model)
-		{
-			if (
-				model == null
-				|| model.Minutes < 0
-				|| model.Minutes > 60
-				|| model.Hours < 0
-				|| model.Hours > 24
-				|| model.Days < 0
-				|| model.Days > 31
-				|| string.IsNullOrWhiteSpace(model.Url)
-				|| string.IsNullOrWhiteSpace(model.BoundaryRegExp)
-			)
-			{
-				return false;
-			}
+    public class DataVerifier
+    {
+        public static bool WebsiteRecordVerifier(WebsiteRecordModel model)
+        {
+            if (
+                model == null
+                || model.Minutes < 0
+                || model.Minutes > 60
+                || model.Hours < 0
+                || model.Hours > 24
+                || model.Days < 0
+                || model.Days > 31
+                || string.IsNullOrWhiteSpace(model.Url)
+                || string.IsNullOrWhiteSpace(model.BoundaryRegExp)
+            )
+            {
+                return false;
+            }
 
-			if (string.IsNullOrWhiteSpace(model.Label))
-			{
-				model.Label = model.Url;
-			}
+            if (string.IsNullOrWhiteSpace(model.Label))
+            {
+                model.Label = model.Url;
+            }
 
-			if (string.IsNullOrWhiteSpace(model.Tags))
-			{
-				model.Tags = "";
-			}
-			return true;
-		}
-	}
+            if (string.IsNullOrWhiteSpace(model.Tags))
+            {
+                model.Tags = "";
+            }
+            return true;
+        }
+
+        public static bool DoesWebsiteRecordExist(int websiteRecordId, string connectionString)
+        {
+            string sql = @"SELECT ""Id"" FROM ""WebsiteRecord"" WHERE ""Id"" = @Id;";
+            var result = DataAccess.LoadData<WebsiteRecordModel, dynamic>(
+                sql,
+                new { Id = websiteRecordId },
+                connectionString
+            );
+            return result.Any();
+        }
+    }
 }
