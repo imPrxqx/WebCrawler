edb8fd19cb2e984a38630fc087a9c62c52543d2e
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-09 14:36:26 +0200

    Update RecordsController.cs returns id for post

--- WebCrawler/Controllers/RecordsController.cs
+++ WebCrawler/Controllers/RecordsController.cs
@@ -6,127 +6,152 @@ using WebCrawler.Models;
 
 namespace WebCrawler.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class RecordsController : ControllerBase
-	{
-		private readonly string _connectionString;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private readonly string _connectionString;
 
-		public RecordsController(ApplicationDbContext context)
-		{
-			Env.Load();
-			_connectionString = GetConnectionString();
-		}
+        public RecordsController(ApplicationDbContext context)
+        {
+            Env.Load();
+            _connectionString = GetConnectionString();
+        }
 
-		private string GetConnectionString()
-		{
-			string databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
-			string databasePass = Environment.GetEnvironmentVariable("DATABASE_PASS");
-			string databaseUser = Environment.GetEnvironmentVariable("DATABASE_USER");
-			string databaseServer = Environment.GetEnvironmentVariable("DATABASE_SERVER");
-			string databasePort = Environment.GetEnvironmentVariable("DATABASE_PORT");
+        private string GetConnectionString()
+        {
+            string databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+            string databasePass = Environment.GetEnvironmentVariable("DATABASE_PASS");
+            string databaseUser = Environment.GetEnvironmentVariable("DATABASE_USER");
+            string databaseServer = Environment.GetEnvironmentVariable("DATABASE_SERVER");
+            string databasePort = Environment.GetEnvironmentVariable("DATABASE_PORT");
 
-			return $"Host={databaseServer};Port={databasePort};Username={databaseUser};Password={databasePass};Database={databaseName};";
-		}
+            return $"Host={databaseServer};Port={databasePort};Username={databaseUser};Password={databasePass};Database={databaseName};";
+        }
 
-		// GET: api/Records
-		[HttpGet]
-		public IEnumerable<WebsiteRecordModel> Get()
-		{
-			
-			string sql =
-				@"
+        // GET: api/Records
+        [HttpGet]
+        public IEnumerable<WebsiteRecordModel> Get()
+        {
+            string sql =
+                @"
         SELECT ""Id"", ""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags""
         FROM public.""WebsiteRecord"";
     ";
 
-			var articles = DataAccess.LoadData<WebsiteRecordModel, dynamic>(
-				sql,
-				new { },
-				_connectionString
-			);
+            var articles = DataAccess.LoadData<WebsiteRecordModel, dynamic>(
+                sql,
+                new { },
+                _connectionString
+            );
 
-			return (articles);
-		}
+            return (articles);
+        }
 
-		// GET api/<RecordsController>/5
-		[HttpGet("{id}")]
-		public WebsiteRecordModel Get(int id)
-		{
-			string sql =
-				@"
+        // GET api/<RecordsController>/5
+        [HttpGet("{id}")]
+        public WebsiteRecordModel Get(int id)
+        {
+            string sql =
+                @"
         SELECT ""Id"", ""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags""
         FROM public.""WebsiteRecord""
         WHERE ""Id"" = @Id;
     ";
 
-			var article = DataAccess
-				.LoadData<WebsiteRecordModel, dynamic>(sql, new { Id = id }, _connectionString)
-				.FirstOrDefault();
+            var article = DataAccess
+                .LoadData<WebsiteRecordModel, dynamic>(sql, new { Id = id }, _connectionString)
+                .FirstOrDefault();
 
-			if (article == null)
-			{
-				return new WebsiteRecordModel();
-			}
+            if (article == null)
+            {
+                return new WebsiteRecordModel();
+            }
 
-			return article;
-		}
+            return article;
+        }
 
-		// POST api/<RecordsController>
-		[HttpPost]
-		public void Post([FromBody] WebsiteRecordModel model)
-		{
-			if (!(model == null))
-			{
-				if (DataVerifier.WebsiteRecordVerifier(model))
-				{
-					string sql =
-						@"
+        // POST api/<RecordsController>
+        [HttpPost]
+        public ActionResult<WebsiteRecordModel> Post([FromBody] WebsiteRecordModel model)
+        {
+            if (!(model == null))
+            {
+                if (DataVerifier.WebsiteRecordVerifier(model))
+                {
+                    string insertSql =
+                        @"
         INSERT INTO public.""WebsiteRecord"" 
-        (""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags"")
-        VALUES (@Url, @BoundaryRegExp, @Days, @Hours, @Minutes, @Label, @IsActive, @Tags);
+        (""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags"", ""LastChange"")
+        VALUES (@Url, @BoundaryRegExp, @Days, @Hours, @Minutes, @Label, @IsActive, @Tags, @LastChange);
     ";
-					try
-					{
-						DataAccess.SaveData(
-							sql,
-							new
-							{
-								model.Url,
-								model.BoundaryRegExp,
-								model.Days,
-								model.Hours,
-								model.Minutes,
-								model.Label,
-								model.IsActive,
-								model.Tags,
-							},
-							_connectionString
-						);
-						Console.WriteLine($"New record created using post");
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine($"Server error: {ex.Message}");
-					}
-				}
-				else
-				{
-					Console.WriteLine($"Record badly filled: {model.Id}");
-				}
-			}
-		}
 
-		// PUT api/<RecordsController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] WebsiteRecordModel model)
-		{
-			if (!(model == null))
-			{
-				if (DataVerifier.WebsiteRecordVerifier(model))
-				{
-					string sql =
-						@"
+                    string selectSql =
+                        @"
+        SELECT ""Id"", ""Url"", ""BoundaryRegExp"", ""Days"", ""Hours"", ""Minutes"", ""Label"", ""IsActive"", ""Tags"", ""LastChange""
+        FROM public.""WebsiteRecord"" 
+        ORDER BY ""Id"" DESC
+        LIMIT 1;
+    ";
+                    try
+                    {
+                        DataAccess.SaveData(
+                            insertSql,
+                            new
+                            {
+                                model.Url,
+                                model.BoundaryRegExp,
+                                model.Days,
+                                model.Hours,
+                                model.Minutes,
+                                model.Label,
+                                model.IsActive,
+                                model.Tags,
+                                model.LastChange,
+                            },
+                            _connectionString
+                        );
+                        var insertedRecord = DataAccess
+                            .LoadData<WebsiteRecordModel, dynamic>(
+                                selectSql,
+                                new { },
+                                _connectionString
+                            )
+                            .FirstOrDefault();
+                        if (insertedRecord != null)
+                        {
+                            Console.WriteLine(
+                                $"New record created and loaded: {insertedRecord.Id}"
+                            );
+                            return Ok(insertedRecord.Id);
+                        }
+                        Console.WriteLine($"New record created using post");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Server error: {ex.Message}");
+                        return StatusCode(500, "Internal server error");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Record badly filled: {model.Id}");
+                    return BadRequest("Invalid record data.");
+                }
+            }
+            return BadRequest("Model is null.");
+        }
+
+        // PUT api/<RecordsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] WebsiteRecordModel model)
+        {
+            if (!(model == null))
+            {
+                if (DataVerifier.WebsiteRecordVerifier(model))
+                {
+                    string sql =
+                        @"
     UPDATE public.""WebsiteRecord"" 
     SET ""Url"" = @Url, 
         ""BoundaryRegExp"" = @BoundaryRegExp, 
@@ -136,74 +161,75 @@ namespace WebCrawler.Controllers
         ""Label"" = @Label, 
         ""IsActive"" = @IsActive, 
         ""Tags"" = @Tags
+, ""LastChange"" =@LastChange
     WHERE ""Id"" = @Id;
 ";
-					try
-					{
-						DataAccess.SaveData(
-							sql,
-							new
-							{
-								Id = id,
-								model.Url,
-								model.BoundaryRegExp,
-								model.Days,
-								model.Hours,
-								model.Minutes,
-								model.Label,
-								model.IsActive,
-								model.Tags,
-							},
-							_connectionString
-						);
-						Console.WriteLine($"Record with Id {id} updated successfully.");
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine($"Server error: {ex.Message}");
-					}
-				}
-				else
-				{
-					Console.WriteLine($"Record badly filled: {model.Id}");
-				}
-			}
-		}
+                    try
+                    {
+                        DataAccess.SaveData(
+                            sql,
+                            new
+                            {
+                                Id = id,
+                                model.Url,
+                                model.BoundaryRegExp,
+                                model.Days,
+                                model.Hours,
+                                model.Minutes,
+                                model.Label,
+                                model.IsActive,
+                                model.Tags,
+                            },
+                            _connectionString
+                        );
+                        Console.WriteLine($"Record with Id {id} updated successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Server error: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Record badly filled: {model.Id}");
+                }
+            }
+        }
 
-		// DELETE api/<RecordsController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-			try
-			{
-				string deleteNodeNeighboursSql =
-					@"
+        // DELETE api/<RecordsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            try
+            {
+                string deleteNodeNeighboursSql =
+                    @"
                     DELETE FROM public.""NodeNeighbours""
                     WHERE ""NodeId"" IN (SELECT ""Id"" FROM public.""Node"" WHERE ""WebsiteRecordId"" = @Id)
                     OR ""NeighbourNodeId"" IN (SELECT ""Id"" FROM public.""Node"" WHERE ""WebsiteRecordId"" = @Id);
                 ";
-				DataAccess.SaveData(deleteNodeNeighboursSql, new { Id = id }, _connectionString);
+                DataAccess.SaveData(deleteNodeNeighboursSql, new { Id = id }, _connectionString);
 
-				string deleteNodesSql =
-					@"
+                string deleteNodesSql =
+                    @"
                     DELETE FROM public.""Node""
                     WHERE ""WebsiteRecordId"" = @Id;
                 ";
-				DataAccess.SaveData(deleteNodesSql, new { Id = id }, _connectionString);
+                DataAccess.SaveData(deleteNodesSql, new { Id = id }, _connectionString);
 
-				string deleteCrawlerSql =
-					@"
+                string deleteCrawlerSql =
+                    @"
                     DELETE FROM public.""WebsiteRecord""
                     WHERE ""Id"" = @Id;
                 ";
-				DataAccess.SaveData(deleteCrawlerSql, new { Id = id }, _connectionString);
+                DataAccess.SaveData(deleteCrawlerSql, new { Id = id }, _connectionString);
 
-				Console.WriteLine($"Record with Id {id} deleted successfully.");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Server error: {ex.Message}");
-			}
-		}
-	}
+                Console.WriteLine($"Record with Id {id} deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Server error: {ex.Message}");
+            }
+        }
+    }
 }
