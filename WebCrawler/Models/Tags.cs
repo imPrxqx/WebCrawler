d82c3ad494cd790b4bcc4f8f26cfce4b49845fb5
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-13 17:51:28 +0200

    Update Tags.cs with prettier

--- WebCrawler/Models/Tags.cs
+++ WebCrawler/Models/Tags.cs
@@ -1,54 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-
 
 namespace WebCrawler.Models
 {
+    class TagsData
+    {
+        protected Dictionary<string, int> allTags = new Dictionary<string, int>();
 
-	class TagsData
-	{
-		protected Dictionary<string, int> allTags = new Dictionary<string, int>();
+        public void AddTags(string tagsInArticle)
+        {
+            string[] tagsNotSorted = tagsInArticle.Split('-');
+            foreach (string tag in tagsNotSorted)
+            {
+                if (!string.IsNullOrEmpty(tag))
+                {
+                    string trimmedTag = tag.Trim();
 
-		public void AddTags(string tagsInArticle)
-		{
-			string[] tagsNotSorted = tagsInArticle.Split('-');
-			foreach (string tag in tagsNotSorted)
-			{
-				if (!string.IsNullOrEmpty(tag))
-				{
-					string trimmedTag = tag.Trim();
+                    if (allTags.ContainsKey(trimmedTag))
+                    {
+                        allTags[trimmedTag]++;
+                        Console.WriteLine(trimmedTag);
+                    }
+                    else
+                    {
+                        allTags[trimmedTag] = 1;
+                        Console.WriteLine("-" + trimmedTag + "-");
+                    }
+                }
+            }
+        }
 
-					if (allTags.ContainsKey(trimmedTag))
-					{
-						allTags[trimmedTag]++;
-						Console.WriteLine(trimmedTag);
-					}
-					else
-					{
-						allTags[trimmedTag] = 1;
-						Console.WriteLine("-" + trimmedTag + "-");
-					}
-				}
-			}
-		}
+        public int OccurrenceTags(string aTag)
+        {
+            string tag = aTag.Trim();
+            if (allTags.ContainsKey(tag))
+            {
+                return allTags[tag];
+            }
+            else
+            {
+                return 0;
+            }
+        }
 
-		public int OccurrenceTags(string aTag)
-		{
-			string tag = aTag.Trim();
-			if (allTags.ContainsKey(tag))
-			{
-				return allTags[tag];
-			}
-			else
-			{
-				return 0;
-			}
-		}
-
-		public Dictionary<string, int> GetAllTags()
-		{
-			return allTags;
-		}
-	}
-
+        public Dictionary<string, int> GetAllTags()
+        {
+            return allTags;
+        }
+    }
 }
