9ed7b796a75f697c43d8b813f7b7f71344de5cd5
matycer <55513549+thematycer@users.noreply.github.com>
2024-09-13 17:47:46 +0200

    Update HomeController.cs with prettier

--- WebCrawler/Controllers/HomeController.cs
+++ WebCrawler/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using WebCrawler.Models;
 
 namespace WebCrawler.Controllers
@@ -26,7 +26,12 @@ namespace WebCrawler.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                }
+            );
         }
     }
 }
