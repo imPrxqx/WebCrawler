d84fc14e2cfc017762a2da84d48179df86be51d7
xmon3r <simon.juza@gmail.com>
2024-09-20 14:15:46 +0200

    Open API Swagger enable

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -23,6 +23,10 @@ namespace WebCrawler
                 .AddType<WebCrawler.GraphQl.Node>()
                 .RegisterService<ApplicationDbContext>();
 
+            //Add Swagger OpenAPI
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             builder.Services.AddSingleton<RecordsQueue>();
 
             builder.Services.AddHostedService<BackgroundObserver>();
@@ -35,6 +39,9 @@ namespace WebCrawler
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
